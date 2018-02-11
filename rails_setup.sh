042fd6a16a86060df1265a98797d81639de030d2
kennpin1 <kennpin1@www10117ui.sakura.ne.jp>
2018-02-11 16:51:41 +0900

    adding --no-ri --no-rdoc

--- rails_setup.sh
+++ rails_setup.sh
@@ -7,12 +7,12 @@ sudo yum -y install libxml2 libxslt libxml2-devel libxslt-devel nginx sqlite sql
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo '"export PATH=$HOME/.rbenv/bin:$PATH"' >> .bash_profile
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile
 echo 'eval "$(rbenv init -)"' >> .bash_profile
 source .bash_profile
 rbenv install 2.3.0
 rbenv global 2.3.0
 gem update
 gem install bundler
-gem install rails --version 5.0.1
+gem install rails --version 5.0.1 --no-ri --no-rdoc
 
